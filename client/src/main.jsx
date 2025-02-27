51fe28ada4eb2e300c733dd0b683a3cdd5df51b8
kaushal1717 <kaushal.padaliya@devxconsultancy.com>
2025-02-27 18:27:18 +0530

    frontend init

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/custom/home";
+import ChatInterface from "./components/custom/ChatInterface";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/chat",
+        element: <ChatInterface />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
