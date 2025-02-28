217f1132bc580272f28594b23214fb1ef7052f46
ratish jain <ratishjain6@gmail.com>
2025-02-28 22:44:49 +0530

    static port

--- server/index.js
+++ server/index.js
@@ -5,8 +5,6 @@ import { config } from "dotenv";
 import router from "./routes.js";
 config();
 
-PORT = 5001;
-
 const app = express();
 app.use(
   cors({
@@ -20,4 +18,4 @@ app.use(express.json());
 
 app.use("/api", router);
 
-app.listen(PORT, () => console.log("✅ Server running on port 5001"));
+app.listen(5001, () => console.log("✅ Server running on port 5001"));
